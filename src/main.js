6ce7caffc25c4da381eee3818cdd4c84e263ad46
Augusto <ampiatto@hotmail.com>
2023-05-06 05:00:50 -0300

    adiciona biblioteca de icones

--- src/main.js
+++ src/main.js
@@ -9,9 +9,18 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
+// Icons
+import mdiVue from "mdi-vue/v3";
+import * as mdijs from "@mdi/js";
+
 const vuetify = createVuetify({
   components,
   directives,
 });
 
-createApp(App).use(store).use(router).use(vuetify).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(vuetify)
+  .use(mdiVue, { icons: mdijs })
+  .mount("#app");
