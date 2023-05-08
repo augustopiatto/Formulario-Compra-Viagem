c66e4928ad72ab437444230e2957b8c264c363f8
Augusto <ampiatto@hotmail.com>
2023-05-07 21:20:42 -0300

    cria os campos e faz validação

---
+++ src/helpers/rules.js
@@ -0,0 +1,22 @@
+const rules = {
+  campoObrigatorio(valor) {
+    return !!valor || "Campo obrigatório";
+  },
+  somenteLetras(valor) {
+    if (!valor) {
+      return;
+    }
+    const regex = /[^a-zA-Z\s]+/;
+    return !regex.test(valor) || "Somente letras";
+  },
+  email(valor) {
+    if (!valor) {
+      return;
+    }
+    const regex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(String(valor).toLowerCase()) || "Digite um e-mail";
+  },
+};
+
+export default rules;
